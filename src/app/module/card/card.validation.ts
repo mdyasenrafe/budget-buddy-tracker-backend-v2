dd8740c8af325898efecd3c54994254216ed5a26
Md yasen rafe <mdyasenrafe@gmail.com>
2025-01-09 02:43:20 +0600

    card validation added

---
+++ src/app/module/card/card.validation.ts
@@ -0,0 +1,19 @@
+import { z } from "zod";
+
+const cardCreateSchema = z.object({
+  last4Digits: z
+    .string()
+    .length(4, { message: "Last 4 digits must be exactly 4 characters" }),
+  bankName: z.string().min(1, { message: "Bank name is required" }),
+  accountHolderName: z
+    .string()
+    .min(1, { message: "Account holder name is required" }),
+  expireDate: z.string(),
+  totalBalance: z.number().nonnegative(),
+  totalDeposit: z.number().nonnegative(),
+  totalExpense: z.number().nonnegative(),
+});
+
+export const cardValidations = {
+  cardCreateSchema,
+};
