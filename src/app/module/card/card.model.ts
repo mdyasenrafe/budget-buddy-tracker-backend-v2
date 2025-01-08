70c93463cb8e4264fb4721535783b5145b6e1fcf
Md yasen rafe <mdyasenrafe@gmail.com>
2025-01-09 02:37:57 +0600

    card model updated

---
+++ src/app/module/card/card.model.ts
@@ -0,0 +1,49 @@
+import mongoose, { Schema } from "mongoose";
+import { TCard } from "./card.type";
+
+const CardSchema: Schema = new Schema<TCard>({
+  userId: {
+    type: Schema.Types.ObjectId,
+    ref: "user",
+    required: true,
+  },
+  last4Digits: {
+    type: String,
+    required: true,
+  },
+  bankName: {
+    type: String,
+    required: true,
+  },
+  accountHolderName: {
+    type: String,
+    required: true,
+  },
+  expireDate: {
+    type: String,
+    required: true,
+  },
+  totalBalance: {
+    type: Number,
+    required: true,
+    min: 0,
+  },
+  totalDeposit: {
+    type: Number,
+    default: 0,
+  },
+  totalExpense: {
+    type: Number,
+    default: 0,
+  },
+});
+
+CardSchema.pre("save", function (next) {
+  if (this.isNew || this.isModified("totalBalance")) {
+    this.totalDeposit = this.totalBalance;
+    this.totalExpense = 0;
+  }
+  next();
+});
+
+export const CardModel = mongoose.model<TCard>("card", CardSchema);
