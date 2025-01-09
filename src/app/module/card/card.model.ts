32412ec1b26335cb31c855bc83f14a33246b7f9c
Md yasen rafe <mdyasenrafe@gmail.com>
2025-01-10 01:42:32 +0600

    time stamps added to card model

--- src/app/module/card/card.model.ts
+++ src/app/module/card/card.model.ts
@@ -1,42 +1,47 @@
 import mongoose, { Schema } from "mongoose";
 import { TCard } from "./card.type";
 
-const CardSchema: Schema = new Schema<TCard>({
-  userId: {
-    type: Schema.Types.ObjectId,
-    ref: "user",
-    required: true,
+const CardSchema: Schema = new Schema<TCard>(
+  {
+    userId: {
+      type: Schema.Types.ObjectId,
+      ref: "user",
+      required: true,
+    },
+    last4Digits: {
+      type: String,
+      required: true,
+    },
+    bankName: {
+      type: String,
+      required: true,
+    },
+    accountHolderName: {
+      type: String,
+      required: true,
+    },
+    expireDate: {
+      type: String,
+      required: true,
+    },
+    totalBalance: {
+      type: Number,
+      required: true,
+      min: 0,
+    },
+    totalDeposit: {
+      type: Number,
+      default: 0,
+    },
+    totalExpense: {
+      type: Number,
+      default: 0,
+    },
   },
-  last4Digits: {
-    type: String,
-    required: true,
-  },
-  bankName: {
-    type: String,
-    required: true,
-  },
-  accountHolderName: {
-    type: String,
-    required: true,
-  },
-  expireDate: {
-    type: String,
-    required: true,
-  },
-  totalBalance: {
-    type: Number,
-    required: true,
-    min: 0,
-  },
-  totalDeposit: {
-    type: Number,
-    default: 0,
-  },
-  totalExpense: {
-    type: Number,
-    default: 0,
-  },
-});
+  {
+    timestamps: true,
+  }
+);
 
 CardSchema.pre("save", function (next) {
   if (this.isNew || this.isModified("totalBalance")) {
