60c69c413f47e37f33b81a492a34a3cbb5239c83
Md yasen rafe <mdyasenrafe@gmail.com>
2025-01-11 03:22:00 +0600

    updated budget model

--- src/app/module/budget/budget.model.ts
+++ src/app/module/budget/budget.model.ts
@@ -1,6 +1,8 @@
 import { Schema, model } from "mongoose";
 import { TBudget } from "./budget.type";
 import { TBudgetMonthValues, TBudgetStatusValues } from "./budget.constant";
+import { AppError } from "../../errors/AppError";
+import httpStatus from "http-status";
 
 const budgetTrackerSchema = new Schema<TBudget>(
   {
@@ -32,5 +34,26 @@ const budgetTrackerSchema = new Schema<TBudget>(
     timestamps: true,
   }
 );
+
+budgetTrackerSchema.pre("save", async function (next) {
+  if (!this.isNew) {
+    return next();
+  }
+
+  const existingBudget = await this.collection.findOne({
+    userId: this.userId,
+    name: this.name,
+    month: this.month,
+  });
+
+  if (existingBudget) {
+    throw new AppError(
+      httpStatus.CONFLICT,
+      `A budget with name '${this.name}' already exists for ${this.month}`
+    );
+  }
+
+  next();
+});
 
 export const BudgetModel = model<TBudget>("budget", budgetTrackerSchema);
