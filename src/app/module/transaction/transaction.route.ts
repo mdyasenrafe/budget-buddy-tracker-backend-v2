4a2602329d475a3b66ecd9b94f832c29bdd49ad5
Md yasen rafe <mdyasenrafe@gmail.com>
2025-01-13 15:56:32 +0600

    transaction route added

---
+++ src/app/module/transaction/transaction.route.ts
@@ -0,0 +1,22 @@
+import express from "express";
+import { validateRequest } from "../../middlewares/validateRequest";
+import { transactionValidations } from "./transaction.validation";
+import { transactionControllers } from "./transaction.controller";
+import { authenticateToken } from "../../middlewares/authMiddleware";
+import { userRolesObject } from "../user/user.constant";
+
+const router = express.Router();
+
+router.post(
+  "/",
+  authenticateToken(userRolesObject.admin, userRolesObject.user),
+  validateRequest(transactionValidations.createTransactionSchema),
+  transactionControllers.createTransaction
+);
+router.get(
+  "/",
+  authenticateToken(userRolesObject.admin, userRolesObject.user),
+  transactionControllers.getTransactions
+);
+
+export const authRoutes = router;
