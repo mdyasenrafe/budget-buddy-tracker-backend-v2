55c04327b24ac213d0897212a39aaa69fd445829
Md yasen rafe <mdyasenrafe@gmail.com>
2025-01-14 01:19:44 +0600

    transaction validation updated

--- src/app/module/transaction/transaction.validation.ts
+++ src/app/module/transaction/transaction.validation.ts
@@ -8,11 +8,12 @@ const createTransactionSchema = z.object({
   title: z.string().min(1, { message: "Title is required" }),
   description: z.string().min(1, { message: "Description is required" }),
   amount: z.number().positive({ message: "Amount must be a positive number" }),
-  date: z.date({ invalid_type_error: "Invalid date format" }),
+  date: z.string(),
   type: z.enum(TTransactionTypeValues, { message: "Invalid transaction type" }),
   category: z.string().min(1, { message: "Category is required" }),
   budget: z.string().optional(),
   card: z.string().optional(),
+  attachment: z.string().optional(),
 });
 
 export const transactionValidations = {
